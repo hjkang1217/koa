fc8447ff52983863967f1f22118f62bb96515421
hyeon <hjkang94@gmail.com>
2021-08-11 02:05:28 +0900

    koa-router 추가

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import * as Koa from 'koa';
-import * as Router from 'koa-router';
+import Koa from 'koa';
+import Router from 'koa-router';
+import api from './api'
+import bodyParser from 'koa-bodyparser';
 
 const app = new Koa();
 const router = new Router();
 const port: number = 8769;
+app.use(bodyParser());
 
 router.get('/', async (ctx, next) => {
     ctx.body = 'Hello World!';
 });
 
+router.use('/api', api.routes());
 app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(port, ()=> {
 	console.log(`Koa server is listening on port ${port}`);
